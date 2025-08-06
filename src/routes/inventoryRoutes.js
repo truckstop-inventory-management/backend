0f931b089626ebf2ae4c364c9b22d7959e1f6aa6
vvayachal <41337015+vvayachal@users.noreply.github.com>
2025-08-06 15:34:52 -0400

    DeleteAll function removed

--- src/routes/inventoryRoutes.js
+++ src/routes/inventoryRoutes.js
@@ -16,6 +16,6 @@ router.get('/', protect, getAllInventory);        // Added route for GET all
 router.get('/:id', protect, getInventoryById);
 router.put('/:id', protect, updateInventory);
 router.delete('/:id', protect, deleteAllInventory);
-router.deleteAllInventory('/', protect, deleteAllInventory);
+// router.deleteAllInventory('/', protect, deleteAllInventory);
 
 export default router;
